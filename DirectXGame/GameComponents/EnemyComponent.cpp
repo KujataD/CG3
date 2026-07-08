0864bcc5ad6636768b76ee0b3457056b754ea950
Otsuka Daichi <109046865+KujataD@users.noreply.github.com>
2026-07-08 17:04:09 +0900

    EnemuBT作成中

--- DirectXGame/GameComponents/EnemyComponent.cpp
+++ DirectXGame/GameComponents/EnemyComponent.cpp
@@ -1,10 +1,38 @@
 #include "EnemyComponent.h"
 
+void EnemyComponent::Initialize() {
+	// BTのアクション登録
+	KujakuEngine::GameObject* owner = GetOwner();
+
+	btFactory_.RegisterAction("MoveForward", [this](BahamutAI::AIContext& context) { return MoveForward(context); });
+	btFactory_.RegisterAction("MoveBackward", [this](BahamutAI::AIContext& context) { return MoveBackward(context); });
+	btFactory_.RegisterAction("RotateY", [this](BahamutAI::AIContext& context) { return RotateY(context); });
+	btFactory_.RegisterAction("RotateX", [this](BahamutAI::AIContext& context) { return RotateX(context); });
+}
+
 void EnemyComponent::Update() {
 	KujakuEngine::GameObject* owner = GetOwner();
 	if (!owner) {
 		return;
 	}
-
-	owner->GetTransform().rotation_.y += speed_;
 }
+
+BahamutAI::BTStatus EnemyComponent::MoveForward(BahamutAI::AIContext& context) {
+	owner_->GetTransform().translation_.z += speed_;
+	if (owner_->GetTransform().translation_.z < 5.0f) {
+		return BahamutAI::BTStatus::Running;
+	}
+	return BahamutAI::BTStatus::Success;
+}
+
+BahamutAI::BTStatus EnemyComponent::MoveBackward(BahamutAI::AIContext& context) {
+	owner_->GetTransform().translation_.z -= speed_;
+	if (owner_->GetTransform().translation_.z > 5.0f) {
+		return BahamutAI::BTStatus::Running;
+	}
+	return BahamutAI::BTStatus::Success;
+}
+
+BahamutAI::BTStatus EnemyComponent::RotateY(BahamutAI::AIContext& context) {}
+
+BahamutAI::BTStatus EnemyComponent::RotateX(BahamutAI::AIContext& context) {}
--- DirectXGame/GameComponents/EnemyComponent.h
+++ DirectXGame/GameComponents/EnemyComponent.h
@@ -1,15 +1,31 @@
 #pragma once
 
 #include <KujakuEngine.h>
+#include <BahamutAI/AI.h>
+
 
 /// <summary>
 /// Enemy用のゲーム固有Component。
 /// </summary>
 class EnemyComponent : public KujakuEngine::Component {
 public:
+enum class Fase{
+	MoveForward,
+	RotateY,
+	MoveBackward,
+	Forward,
+};
+public:
 	const char* GetTypeName() const override { return "EnemyComponent"; }
 
+	void Initialize() override;
 	void Update() override;
+
+private:
+	BahamutAI::BTStatus MoveForward(BahamutAI::AIContext& context);
+	BahamutAI::BTStatus RotateY(BahamutAI::AIContext& context);
+	BahamutAI::BTStatus MoveBackward(BahamutAI::AIContext& context);
+	BahamutAI::BTStatus RotateX(BahamutAI::AIContext& context);
 
 private:
 	KUJAKU_SERIALIZED_FIELDS_BEGIN() {
@@ -17,4 +33,7 @@ private:
 	}
 
 	KUJAKU_FIELD_FLOAT(speed_, 0.02f);
+
+	// BT作成用
+	BahamutAI::BehaviorTreeFactory btFactory_;
 };
